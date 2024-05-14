e1cdb83ffad4596f9c3c73062aca571538652a46
auttmme <ichautm14@gmail.com>
2024-05-15 00:29:17 +0700

    imp: fix schedule card and page

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -1,76 +1,169 @@
 import { useQuery } from "@tanstack/react-query";
 import { getEvents } from "../Api/getEvents";
 import { useState } from "react";
-import { Box, Button, Flex, Spinner, Stack } from "@chakra-ui/react";
+import {
+	Box,
+	Button,
+	Flex,
+	Spinner,
+	Stack,
+	Text,
+	Heading,
+	Input,
+	Card,
+} from "@chakra-ui/react";
 import EventCard from "../Components/Events/EventCard";
-import { useNavigate } from "react-router-dom";
+import { Divider } from "@chakra-ui/react";
+
+import Navbar from "../Components/Layout/Navbar";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 
 function Schedules() {
-	const navigate = useNavigate();
-	const [limit, setLimit] = useState(5);
+	const [page, setPage] = useState(1);
 
-	const handleLimit = () => {
-		setLimit((prevLimit) => prevLimit + 5);
-	};
-
-	const { data, isFetching } = useQuery({
-		queryKey: ["GetEvents", 1, limit],
-		queryFn: () => getEvents(1, limit),
+	const { data, isLoading } = useQuery({
+		queryKey: ["GetEvents", page, 5],
+		queryFn: () => getEvents(page, 5),
 	});
 
 	console.log("data", data);
 
+	const handlePrev = () => {
+		if (page > 1) {
+			setPage((prevPage) => prevPage - 1);
+		}
+	};
+
+	const handleNext = () => {
+		if (page < 5) {
+			setPage((prevPage) => prevPage + 1);
+		}
+	};
+
 	return (
 		<Stack gap={0}>
 			<Box
-				background="url('/public/bg_schedule_header.png')"
+				background="url('/bg_schedule_header.png')"
 				objectFit={"cover"}
 				width="100%"
-				height={{ base: "200px", md: "300px", xl: "500px" }}
+				height={{ base: "200px", md: "300px", xl: "663px" }}
 			>
-				HEADER
+				<Navbar />
+				<Stack mt="143px" ml="350px" mb="336px" color="white">
+					<Heading>Schedule Page</Heading>
+					<Box borderLeft={"1px solid #F84A43"} pl={2} fontWeight={"600"}>
+						A comprehensive overview of all the upcoming events
+					</Box>
+				</Stack>
 			</Box>
 			<Flex
 				justifyContent={"center"}
 				alignItems={"center"}
-				background="url('/public/bg_schedule_body.png')"
+				background="url('/bg_schedule_body.png')"
 				backgroundRepeat={"no-repeat"}
-				py={10}
 			>
-				<Box>
-					{data?.map((item) => (
-						<Box
-							mb={8}
-							onClick={() => navigate(`/event-detail/${item.id}`)}
-							cursor={"pointer"}
-						>
-							<EventCard
-								eventDescription={item.eventDescription}
-								eventName={item.eventName}
-								eventBannerImageUrl={item.eventBannerImageUrl}
-								eventCity={item.eventCity}
-								eventCountry={item.eventCountry}
-								eventDate={item.eventDate}
-								key={item.id}
-							/>
-						</Box>
-					))}
-					<Button
-						onClick={() => handleLimit()}
-						isDisabled={limit === 50}
-						color={"white"}
-						borderRadius={"32px"}
-						w={{ base: "300px", md: "500px", xl: "700px" }}
-						py="7"
-						bgGradient="linear(to-l, #FF7848, #F94B44)"
-						_hover={{
-							bgGradient: "linear(to-l, #FF7848, #F94B44)",
-							color: "white",
-						}}
+				{isLoading ? (
+					<Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"}>
+						<Spinner
+							thickness="4px"
+							speed="0.65s"
+							emptyColor="gray.200"
+							color="blue.500"
+							size="xl"
+						/>
+					</Flex>
+				) : (
+					<Card
+						variant={"elevated"}
+						maxW={"1143px"}
+						w={"1143px"}
+						rounded={"xl"}
+						boxShadow={"xl"}
+						my={8}
 					>
-						{isFetching ? <Spinner /> : "VIEW MORE"}
-					</Button>
-				</Box>
+						<Stack>
+							<Flex
+								bgColor={"#2E2E2E"}
+								color={"white"}
+								p={8}
+								alignItems="center"
+								gap={6}
+							>
+								<Text>Search Your Event: </Text>
+								<Input
+									variant="filled"
+									bgColor={"white"}
+									w={"30%"}
+									rounded={"28px"}
+									placeholder="Please Type Event Name"
+									_placeholder={{
+										color: "#b3b3b3",
+									}}
+								/>
+								<Button
+									bgGradient="linear(to-l, #7D7D7D, #2F2E2E)"
+									color={"white"}
+									borderRadius={"32px"}
+									px={6}
+									py={6}
+								>
+									Search
+								</Button>
+							</Flex>
+							<Flex
+								direction={"column"}
+								alignItems="center"
+								justifyContent={"center"}
+								gap={8}
+								mt={4}
+							>
+								{data?.map((item) => (
+									<>
+										<EventCard
+											id={item.id}
+											eventDescription={item.eventDescription}
+											eventName={item.eventName}
+											eventBannerImageUrl={item.eventBannerImageUrl}
+											eventCity={item.eventCity}
+											eventCountry={item.eventCountry}
+											eventDate={item.eventDate}
+											key={item.id}
+										/>
+										<Divider />
+									</>
+								))}
+							</Flex>
+							<Flex justifyContent={"center"} gap={10} mb={4}>
+								<Button
+									onClick={() => handlePrev()}
+									isDisabled={page === 1}
+									color={"white"}
+									borderRadius={"32px"}
+									bgGradient="linear(to-l, #FF7848, #F94B44)"
+									_hover={{
+										bgGradient: "linear(to-l, #FF7848, #F94B44)",
+										color: "white",
+									}}
+								>
+									<ChevronLeftIcon w={8} h={8} />
+								</Button>
+								<Button
+									onClick={() => handleNext()}
+									isDisabled={page === 5}
+									color={"white"}
+									borderRadius={"32px"}
+									bgGradient="linear(to-l, #FF7848, #F94B44)"
+									_hover={{
+										bgGradient: "linear(to-l, #FF7848, #F94B44)",
+										color: "white",
+									}}
+								>
+									<ChevronRightIcon w={8} h={8} />
+								</Button>
+							</Flex>
+						</Stack>
+					</Card>
+				)}
 			</Flex>
 		</Stack>
 	);
