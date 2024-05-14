358a6f9c44ccf8eb5e29253d8ab3a3982fe75711
auttmme <ichautm14@gmail.com>
2024-05-14 19:54:49 +0700

    new: create layout

---
+++ src/Components/Layout/Footer.tsx
@@ -0,0 +1,31 @@
+import { Box } from "@chakra-ui/react";
+
+function Footer() {
+	return (
+		<Box
+			position="relative"
+			background="url('/public/footer_bg.jpeg')"
+			backgroundPosition="center"
+			backgroundRepeat="no-repeat"
+			backgroundSize="cover"
+			height={{ base: "200px", md: "300px", xl: "500px" }}
+			_before={{
+				content: '""',
+				position: "absolute",
+				top: 0,
+				left: 0,
+				width: "100%",
+				height: "100%",
+				background:
+					"linear-gradient(270deg, rgba(255, 0, 0, 0.4) 0%, rgba(143, 0, 255, 0.4) 100%)",
+				zIndex: 1,
+			}}
+		>
+			<Box position="relative" zIndex={2}>
+				Footer
+			</Box>
+		</Box>
+	);
+}
+
+export default Footer;
